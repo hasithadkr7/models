221729c7f993f67e871f6e01f814bcc06cc5389d
nirandaperera <niranda.perera@gmail.com>
2017-05-15 13:12:46 +0530

    adding some routines

--- wrf/execution/executor.py
+++ wrf/execution/executor.py
@@ -1,10 +1,14 @@
 import logging
+import os
 import threading
 import time
+import datetime as dt
+import pkg_resources
+import subprocess
 import wget
 import sys
 
-from wrf.execution import constants, utils
+from wrf.execution import constants, utils, exceptions
 
 
 def download_single_inventory(url, dest, retries=5, delay=10):
@@ -24,8 +28,7 @@ def download_single_inventory(url, dest, retries=5, delay=10):
             try_count += 1
             time.sleep(delay)
 
-    logging.error('Unable to download %s' % url)
-    return False
+    raise exceptions.UnableToDownloadGfsData(url)
 
 
 class InventoryDownloadThread(threading.Thread):
@@ -38,12 +41,12 @@ class InventoryDownloadThread(threading.Thread):
         self.delay = delay
 
     def run(self):
-
-        logging.debug('Downloading from thread %d: START' % self.thread_id)
-        if download_single_inventory(self.url, self.dest, self.retries, self.delay):
+        try:
+            logging.debug('Downloading from thread %d: START' % self.thread_id)
+            download_single_inventory(self.url, self.dest, self.retries, self.delay)
             logging.debug('Downloading from thread %d: END' % self.thread_id)
-        else:
-            logging.debug('Error in downloading from thread %d' % self.thread_id)
+        except exceptions.UnableToDownloadGfsData as e:
+            logging.error('Error in downloading from thread %d' % self.thread_id)
 
 
 def download_gfs_data(date, gfs_dir,
@@ -63,7 +66,7 @@ def download_gfs_data(date, gfs_dir,
         logging.info('Cleaning the GFS dir: %s' % gfs_dir)
         utils.cleanup_dir(gfs_dir)
 
-    inventories = utils.get_gfs_inventory_dest_list(url, inv, date, period, step, cycle, res, gfs_dir)
+    inventories = utils.get_gfs_inventory_url_dest_list(url, inv, date, period, step, cycle, res, gfs_dir)
     logging.info(
         'Following data will be downloaded with %d parallel threads\n%s' % (
             thread_count, '\n'.join(' '.join(map(str, i)) for i in inventories)))
@@ -88,16 +91,124 @@ def download_gfs_data(date, gfs_dir,
     logging.info('Downloading GFS data: END Elapsed time: %f' % elapsed_time)
 
 
-def run_wrf():
-    logging.info('Running WRF')
-    # some logic to run wrf
-    pass
+def check_gfs_data_availability(wrf_home, start_date, inv, period, step, cycle, res):
+    logging.info('Checking gfs data availability...')
+    inventories = utils.get_gfs_inventory_dest_list(inv, start_date, period, step, cycle, res,
+                                                    utils.get_gfs_dir(wrf_home))
+    missing_inv = []
+    for inv in inventories:
+        if not os.path.exists(inv):
+            missing_inv.append(inv)
+
+    if len(missing_inv) > 0:
+        logging.error('Some data unavailable')
+        raise exceptions.GfsDataUnavailable('Some data unavailable', missing_inv)
+
+    logging.info('GFS data available')
+
+
+def run_wps(wrf_home, start_date):
+    logging.info('Running WPS...')
+    wps_dir = utils.get_wps_dir(wrf_home)
+
+    logging.info('Cleaning up files')
+    utils.delete_files_with_prefix(wps_dir, 'FILE:*')
+    utils.delete_files_with_prefix(wps_dir, 'PFILE:*')
+    utils.delete_files_with_prefix(wps_dir, 'met_em*')
+
+    # Linking VTable
+    utils.run_subprocess('ln -sf %s/ungrib/Variable_Tables/Vtable.NAM %s/Vtable' % (wps_dir, wps_dir))
+
+    # Running link_grib.csh
+    utils.run_subprocess(
+        'csh %s/link_grib.csh %s/%s' % (wps_dir, utils.get_gfs_dir(wrf_home), start_date.strftime('%Y%m%d')))
+
+    # Starting ungrib.exe
+    utils.run_subprocess('./%s/ungrib.exe' % wps_dir)
+
+    # Starting geogrid.exe'
+    utils.run_subprocess('./%s/geogrid.exe' % wps_dir)
+
+    # Starting metgrid.exe'
+    utils.run_subprocess('./%s/metgrid.exe' % wps_dir)
+
+
+def replace_namelist_wps(wrf_home, start_date, end_date):
+    logging.info('Replacing namelist.wps...')
+    wps = pkg_resources.resource_filename(__name__, 'conf/namelist.wps')
+    d = {
+        'YYYY1': start_date.strftime('%Y'),
+        'MM1': start_date.strftime('%m'),
+        'DD1': start_date.strftime('%d'),
+        'YYYY2': end_date.strftime('%Y'),
+        'MM2': end_date.strftime('%m'),
+        'DD2': end_date.strftime('%d'),
+        'GEOG': utils.get_geog_dir(wrf_home)
+    }
+    utils.replace_file_with_values(wps, os.path.join(wrf_home, utils.get_wps_dir(wrf_home), 'namelist.wps'), d)
+
+
+def replace_namelist_input(wrf_home, start_date, end_date):
+    logging.info('Replacing namelist.input ...')
+    f = pkg_resources.resource_filename(__name__, 'conf/namelist.input')
+    d = {
+        'YYYY1': start_date.strftime('%Y'),
+        'MM1': start_date.strftime('%m'),
+        'DD1': start_date.strftime('%d'),
+        'YYYY2': end_date.strftime('%Y'),
+        'MM2': end_date.strftime('%m'),
+        'DD2': end_date.strftime('%d'),
+    }
+    utils.replace_file_with_values(f, os.path.join(wrf_home, utils.get_em_real_dir(wrf_home), 'namelist.input'), d)
+
+
+def run_em_real(wrf_home, start_date, procs):
+    logging.info('Running em_real...')
+    em_real_dir = utils.get_em_real_dir(wrf_home)
+
+    logging.info('Cleaning up files')
+    utils.delete_files_with_prefix(em_real_dir, 'met_em*')
+    utils.delete_files_with_prefix(em_real_dir, 'rsl*')
+
+    # Linking met_em.*
+    utils.run_subprocess('ln -sf %s/met_em.d0* %s/' % (utils.get_wps_dir(wrf_home), em_real_dir))
+
+    # Starting real.exe
+    utils.run_subprocess('mpirun -np %d ./%s/real.exe' % (procs, em_real_dir))
+    utils.move_files_with_prefix(em_real_dir, 'rsl*', os.path.join(utils.get_logs_dir(wrf_home),
+                                                                   'rsl-real-%s' % start_date.strftime('%Y%m%d')))
+
+    # Starting wrf.exe'
+    utils.run_subprocess('mpirun -np %d ./%s/wrf.exe' % (procs, em_real_dir))
+    utils.move_files_with_prefix(em_real_dir, 'rsl*', os.path.join(utils.get_logs_dir(wrf_home),
+                                                                   'rsl-wrf-%s' % start_date.strftime('%Y%m%d')))
+
+
+def run_wrf(wrf_home, start_date, procs=constants.DEFAULT_PROCS,
+            inv=constants.DEFAULT_GFS_DATA_INV,
+            period=constants.DEFAULT_PERIOD,
+            step=constants.DEFAULT_STEP,
+            cycle=constants.DEFAULT_CYCLE,
+            res=constants.DEFAULT_RES):
+    end_date = start_date + dt.timedelta(days=period)
+
+    logging.info('Running WRF from %s to %s...' % (start_date.strftime('%Y%m%d'), end_date.strftime('%Y%m%d')))
+
+    check_gfs_data_availability(wrf_home, start_date, inv, period, step, cycle, res)
+
+    replace_namelist_wps(wrf_home, start_date, end_date)
+    run_wps(wrf_home, start_date)
+
+    replace_namelist_input(wrf_home, start_date, end_date)
+    run_em_real(wrf_home, start_date, procs)
 
 
 def wrf_run_all(wrf_home, start_date, end_date, period, meta_data=list()):
-    logging.info('Running all WRF')
+    logging.info('Downloading GFS Data')
     download_gfs_data(start_date, utils.get_gfs_dir(wrf_home), period=period)
 
+    logging.info('Running WRF')
+    run_wrf(wrf_home, start_date, period=period)
     # some logic to download data and run wrf
 
 
--- wrf/execution/utils.py
+++ wrf/execution/utils.py
@@ -3,11 +3,16 @@ import datetime as dt
 import glob
 import logging
 import logging.config
-
+import re
 import shutil
+import time
+import ntpath
+
+
+import os
+import subprocess
 
 import constants as constants
-import os
 
 
 def parse_args():
@@ -58,6 +63,18 @@ def get_gfs_dir(wrf_home=constants.DEFAULT_WRF_HOME):
     return create_dir_if_not_exists(os.path.join(wrf_home, 'DATA', 'GFS'))
 
 
+def get_wps_dir(wrf_home=constants.DEFAULT_WRF_HOME):
+    return os.path.join(wrf_home, constants.DEFAULT_WPS_PATH)
+
+
+def get_em_real_dir(wrf_home=constants.DEFAULT_WRF_HOME):
+    return os.path.join(wrf_home, constants.DEFAULT_EM_REAL_PATH)
+
+
+def get_geog_dir(wrf_home=constants.DEFAULT_WRF_HOME):
+    return os.path.join(wrf_home, 'DATA', 'geog')
+
+
 def get_output_dir(wrf_home=constants.DEFAULT_WRF_HOME):
     return create_dir_if_not_exists(os.path.join(wrf_home, 'OUTPUT'))
 
@@ -77,10 +94,75 @@ def get_gfs_data_url_dest_tuple(url, inv, date_str, cycle, fcst_id, res, gfs_dir
     return url0 + inv0, dest
 
 
-def get_gfs_inventory_dest_list(url, inv, date, period, step, cycle, res, gfs_dir):
+def get_gfs_data_dest(inv, date_str, cycle, fcst_id, res, gfs_dir):
+    inv0 = inv.replace('CC', cycle).replace('FFF', fcst_id).replace('RRRR', res)
+    dest = os.path.join(gfs_dir, date_str + '.' + inv0)
+    return dest
+
+
+def get_gfs_inventory_url_dest_list(url, inv, date, period, step, cycle, res, gfs_dir):
     date_str = date.strftime('%Y%m%d')
     return [get_gfs_data_url_dest_tuple(url, inv, date_str, cycle, str(i).zfill(3), res, gfs_dir) for i in
             range(0, period * 24 + 1, step)]
+
+
+def get_gfs_inventory_dest_list(inv, date, period, step, cycle, res, gfs_dir):
+    date_str = date.strftime('%Y%m%d')
+    return [get_gfs_data_dest(inv, date_str, cycle, str(i).zfill(3), res, gfs_dir) for i in
+            range(0, period * 24 + 1, step)]
+
+
+def replace_file_with_values(source, destination, val_dict):
+    logging.debug('replace file source ' + source)
+    logging.debug('replace file destination ' + destination)
+    logging.debug('replace file content dict ' + str(val_dict))
+
+    # pattern = re.compile(r'\b(' + '|'.join(val_dict.keys()) + r')\b')
+    pattern = re.compile('|'.join(val_dict.keys()))
+
+    dest = open(destination, 'w')
+    out = ''
+    with open(source, 'r') as src:
+        line = pattern.sub(lambda x: val_dict[x.group()], src.read())
+        dest.write(line)
+        out += line
+
+    dest.close()
+    logging.debug('replace file final content \n' + out)
+
+
+def cleanup_dir(gfs_dir):
+    shutil.rmtree(gfs_dir)
+    os.makedirs(gfs_dir)
+
+
+def delete_files_with_prefix(directory, prefix):
+    for filename in glob.glob(os.path.join(directory, prefix)):
+        os.remove(filename)
+
+
+def move_files_with_prefix(directory, prefix, dest):
+    create_dir_if_not_exists(dest)
+    for filename in glob.glob(os.path.join(directory, prefix)):
+        os.rename(filename, os.path.join(dest, ntpath.basename(filename)))
+
+
+def run_subprocess(cmd):
+    logging.info('Running subprocess %s' % cmd)
+    start_t = time.time()
+    output = ''
+    try:
+        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        logging.error('Exception in subprocess %s! Error code %d' % (cmd, e.returncode))
+        logging.error(e.output)
+        raise e
+    finally:
+        elapsed_t = time.time() - start_t
+        logging.info('Subprocess %s finished in %f s' % (cmd, elapsed_t))
+        logging.debug('stdout and stderr of %s\n%s' % (cmd, output))
+
+    return output
 
 
 def main():
@@ -92,19 +174,21 @@ def main():
     print get_gfs_data_url_dest_tuple(constants.DEFAULT_GFS_DATA_URL, constants.DEFAULT_GFS_DATA_INV,
                                       get_gfs_dir(wrf_home), '20170501', constants.DEFAULT_CYCLE, '001',
                                       constants.DEFAULT_RES)
-    print get_gfs_inventory_dest_list(constants.DEFAULT_GFS_DATA_URL, constants.DEFAULT_GFS_DATA_INV,
-                                      dt.datetime.strptime('2017-05-01', '%Y-%m-%d'),
-                                      constants.DEFAULT_PERIOD,
-                                      constants.DEFAULT_STEP,
-                                      constants.DEFAULT_CYCLE,
-                                      constants.DEFAULT_RES,
-                                      get_gfs_dir(wrf_home))
+    print get_gfs_inventory_url_dest_list(constants.DEFAULT_GFS_DATA_URL, constants.DEFAULT_GFS_DATA_INV,
+                                          dt.datetime.strptime('2017-05-01', '%Y-%m-%d'),
+                                          constants.DEFAULT_PERIOD,
+                                          constants.DEFAULT_STEP,
+                                          constants.DEFAULT_CYCLE,
+                                          constants.DEFAULT_RES,
+                                          get_gfs_dir(wrf_home))
+    d = {
+        'YYYY1': '2016',
+        'MM1': '05',
+        'DD1': '01'
+    }
+
+    print replace_file_with_values('conf/namelist.input', wrf_home + '/namelist.wps', d)
 
 
 if __name__ == "__main__":
     main()
-
-
-def cleanup_dir(gfs_dir):
-    shutil.rmtree(gfs_dir)
-    os.makedirs(gfs_dir)
