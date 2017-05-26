ab0ad64cff3b20a1a4171aa5a7daf7b9c807813e
nirandaperera <niranda.perera@gmail.com>
2017-05-26 14:12:31 +0530

    adding multiple watchdogs to each station

--- curwrf/realtime/data_read.py
+++ curwrf/realtime/data_read.py
@@ -8,23 +8,27 @@ import sys
 import pandas as pd
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
+import threading
+
+threadLock = threading.Lock()
 
 
 class DataEventHandler(FileSystemEventHandler):
-    def __init__(self, out_file, prev_file=None):
+    def __init__(self, start_str, out_file, prev_file=None):
+        self.start_str = start_str
         self.out_file = out_file
         self.prev_file = prev_file
 
     def on_created(self, event):
         logging.info("File created %s" % event)
-        if event.src_path.endswith('.dat') and event.src_path.split('/')[-1].startswith('CR200_'):
+        if event.src_path.split('/')[-1].startswith(self.start_str) and event.src_path.endswith('.dat'):
             logging.info('CR200_*.dat file created')
             process_sat_file(event.src_path, self.out_file, self.prev_file)
             self.prev_file = event.src_path
 
     def on_moved(self, event):
         logging.info("File moved/renamed %s" % event)
-        if event.dest_path.endswith('.dat') and event.dest_path.split('/')[-1].startswith('CR200_'):
+        if event.src_path.split('/')[-1].startswith(self.start_str) and event.src_path.endswith('.dat'):
             logging.info('file renamed to CR200_*.dat')
             process_sat_file(event.dest_path, self.out_file, self.prev_file)
             self.prev_file = event.dest_path
@@ -45,8 +49,10 @@ def process_sat_file(src_file, dest_file, prv_src_file=None):
         columns={"sum": "rainfall", "count": "samples"})
     means['STATION'] = station
 
+    threadLock.acquire()
     with open(dest_file, 'a') as f:
         means[0 if prv_src_file is None else 1:len(means) - 1].to_csv(f, header=False, date_format='%Y-%m-%d_%H:%M:%S')
+    threadLock.release()
 
 
 def process_old_files(src, dest_file):
@@ -73,7 +79,7 @@ def main(argv=None):
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(threadName)s %(module)s %(levelname)s %(message)s')
     path = argv[1]
     logging.info('data dir %s' % path)
-    process_old = bool(argv[2]) if len(argv) > 2 else False
+    process_old = int(argv[2]) if len(argv) > 2 else False
     logging.info('process old %s' % str(process_old))
     dest_file = argv[3] if len(argv) > 3 else 'summary.txt'
     logging.info('dest file %s' % dest_file)
@@ -83,8 +89,10 @@ def main(argv=None):
         process_old_files(path, dest_file)
 
     observer = Observer()
-    event_handler = DataEventHandler(dest_file)
-    observer.schedule(event_handler, path, recursive=False)
+    for i in range(6):
+        event_handler = DataEventHandler('CR200_KALU0%d' % (i + 1), dest_file)
+        observer.schedule(event_handler, path, recursive=False)
+
     observer.start()
     observer.join()
 
