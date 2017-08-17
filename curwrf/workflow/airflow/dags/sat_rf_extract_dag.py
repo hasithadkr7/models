4dbc86998143024b6c58ea449117dc22c7dd7d6d
niranda <niranda.perera@gmail.com>
2017-08-17 15:58:18 +0530

    adding sat extractor

---
+++ curwrf/workflow/airflow/dags/sat_rf_extract_dag.py
@@ -0,0 +1,45 @@
+import airflow
+import datetime as dt
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+
+sat_dag_name = 'sat_rf_extraction'
+queue = 'wrf_fs_impl_queue'
+schedule_interval = '@hourly'
+curw_py_dir = '/opt/git/models'
+output_dir = '/mnt/disks/curwsl_nfs/sat'
+# curw_py_dir = '/home/curw/git/models'
+# output_dir = '/home/curw/temp'
+
+default_args = {
+    'owner': 'curwsl admin',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(0),
+    'email': ['admin@curwsl.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 6,
+    'retry_delay': dt.timedelta(minutes=15),
+    'queue': queue,
+}
+
+# initiate the DAG
+dag = DAG(
+    sat_dag_name,
+    default_args=default_args,
+    description='Extracting JAXA satellite rainfall data hourly',
+    schedule_interval=schedule_interval)
+
+cmd_str = 'python3 %s/curwrf/wrf/extraction/sat_extractor.py ' \
+          '-start {{ macros.datetime.strftime(execution_date - macros.timedelta(hours=1), \'%%Y-%%m-%%d_%%H:%%M\') }} ' \
+          '-end {{ execution_date.strftime(\'%%Y-%%m-%%d_%%H:%%M\') }} ' \
+          '-output %s' % (curw_py_dir, output_dir)
+
+BashOperator(
+    task_id='sat_rf_extraction',
+    bash_command=cmd_str,
+    default_args=default_args,
+    dag=dag
+)
--- curwrf/wrf/extraction/sat_extractor.py
+++ curwrf/wrf/extraction/sat_extractor.py
@@ -16,7 +16,13 @@ from joblib import Parallel, delayed
 from mpl_toolkits.basemap import cm
 
 
-def extract_jaxa_satellite_data(start_ts_utc, end_ts_utc, output_dir, cleanup=True):
+def extract_jaxa_satellite_hourly_data(ts, output_dir):
+    ts = utils.datetime_floor(ts, 3600)
+    logging.info('Jaxa satellite data extraction for %s (previous hour)' % str(ts))
+    extract_jaxa_satellite_data(ts - dt.timedelta(hours=1), ts, output_dir)
+
+
+def extract_jaxa_satellite_data(start_ts_utc, end_ts_utc, output_dir, cleanup=True, cum=False):
     start = utils.datetime_floor(start_ts_utc, 3600)
     end = utils.datetime_floor(end_ts_utc, 3600)
 
@@ -62,9 +68,10 @@ def extract_jaxa_satellite_data(start_ts_utc, end_ts_utc, output_dir, cleanup=Tr
         delayed(process_jaxa_zip_file)(i[1], i[2], lat_min, lon_min, lat_max, lon_max, True) for i in url_dest_list)
     logging.info('Processing files complete')
 
-    logging.info('Processing cumulative')
-    process_cumulative_plot(url_dest_list, start_ts_utc, end_ts_utc, output_dir, lat_min, lon_min, lat_max, lon_max)
-    logging.info('Processing cumulative complete')
+    if cum:
+        logging.info('Processing cumulative')
+        process_cumulative_plot(url_dest_list, start_ts_utc, end_ts_utc, output_dir, lat_min, lon_min, lat_max, lon_max)
+        logging.info('Processing cumulative complete')
 
     # clean up temp dir
     if cleanup:
@@ -86,7 +93,8 @@ def process_cumulative_plot(url_dest_list, start_ts_utc, end_ts_utc, output_dir,
                 total += np.genfromtxt(url_dest[2] + '.archive', dtype=float)
         title = 'Cumulative rainfall ' + from_to
         clevs = np.concatenate(([-1, 0], np.array([pow(2, i) for i in range(0, 9)])))
-        ext_utils.create_contour_plot(total, cum_filename, lat_min, lon_min, lat_max, lon_max, title, clevs=clevs, cmap=cm.s3pcpn)
+        ext_utils.create_contour_plot(total, cum_filename, lat_min, lon_min, lat_max, lon_max, title, clevs=clevs,
+                                      cmap=cm.s3pcpn)
     else:
         logging.info('%s already exits' % cum_filename)
 
@@ -107,7 +115,7 @@ def process_jaxa_zip_file(zip_file_path, out_file_path, lat_min, lon_min, lat_ma
     clevs = np.concatenate(([-1, 0], np.array([pow(2, i) for i in range(0, 9)])))
     title = 'Hourly rainfall ' + os.path.basename(out_file_path).replace('jaxa_sat_rf_', '').replace('.asc', '')
     ext_utils.create_contour_plot(data, out_file_path + '.png', lat_min, lon_min, lat_max, lon_max, title, clevs=clevs,
-                            cmap=cm.s3pcpn)
+                                  cmap=cm.s3pcpn)
 
     if archive_data and not utils.file_exists_nonempty(out_file_path + '.archive'):
         np.savetxt(out_file_path + '.archive', data, fmt='%g')
@@ -125,6 +133,7 @@ if __name__ == "__main__":
                         help='End timestamp UTC with format %%Y-%%m-%%d_%%H:%%M', dest='end_ts')
     parser.add_argument('-output', default=None, help='Output directory of the images', dest='output')
     parser.add_argument('-clean', default=0, help='Cleanup temp directory', dest='clean', type=int)
+    parser.add_argument('-cum', default=0, help='Process cumulative plot', dest='cum', type=int)
     args = parser.parse_args()
 
     if args.output is None:
@@ -134,4 +143,4 @@ if __name__ == "__main__":
 
     extract_jaxa_satellite_data(dt.datetime.strptime(args.start_ts, '%Y-%m-%d_%H:%M'),
                                 dt.datetime.strptime(args.end_ts, '%Y-%m-%d_%H:%M'),
-                                output, bool(args.clean))
+                                output, cleanup=bool(args.clean), cum=bool(args.cum))
