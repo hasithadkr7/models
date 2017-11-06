fb840ca5c6bce19de9a03768693cfe369968aee0
nirandaperera <niranda.perera@gmail.com>
2017-11-06 09:23:58 +0530

    minor change in runwrf sh

--- curw/container/docker/rainfall/ncar_wrf/run_wrf.sh
+++ curw/container/docker/rainfall/ncar_wrf/run_wrf.sh
@@ -2,7 +2,7 @@
 
 export GOOGLE_APPLICATION_CREDENTIALS=/wrf/gcs.json
 
-echo "Reading running args..."
+echo "#### Reading running args..."
 while getopts ":i:c:m:x:y:k:v:" option
 do
  case "${option}"
@@ -15,15 +15,15 @@ do
  k) echo $OPTARG > /wrf/gcs.json;;
  v) bucket=$(echo $OPTARG | cut -d':' -f1)
     path=$(echo $OPTARG | cut -d':' -f2)
-    echo "mounting $bucket to $path"
+    echo "#### mounting $bucket to $path"
     gcsfuse ${bucket} ${path} ;;
  esac
 done
 
-echo "Pulling curwsl changes..."
+echo "#### Pulling curwsl changes..."
 cd /wrf/curwsl
 git pull
 cd /wrf
 
-echo "Running the data extraction procedures..."
+echo "#### Running the data extraction procedures..."
 python3.6 /wrf/curwsl/curw/container/docker/rainfall/ncar_wrf/run_wrf.py -run_id="$ID" -wrf_config="$CONFIG" -mode="$MODE" -nl_wps="$WPS" -nl_input="$INPUT"
