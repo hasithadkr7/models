17a177ec9950f218da58814306fb3cc8a5664cf8
nirandaperera <niranda.perera@gmail.com>
2018-04-04 16:49:41 +0530

    adding loop operator

--- curw/workflow/airflow/extensions/operators/curw_dagrun_operator.py
+++ curw/workflow/airflow/extensions/operators/curw_dagrun_operator.py
@@ -1,11 +1,14 @@
-from datetime import datetime
+from sqlalchemy import select, and_, update
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.schema import MetaData
 
-from airflow.operators.dagrun_operator import DagRunOrder
-
+from airflow import settings
 from airflow.models import BaseOperator, DagBag, SkipMixin
+from airflow.operators.dagrun_operator import DagRunOrder
 from airflow.utils.decorators import apply_defaults
 from airflow.utils.state import State
-from airflow import settings
+
+Base = declarative_base()
 
 
 class CurwTriggerDagRunOperator(BaseOperator, SkipMixin):
@@ -34,29 +37,124 @@ class CurwTriggerDagRunOperator(BaseOperator, SkipMixin):
             self,
             trigger_dag_id,
             python_callable,
+            loop_id,
             skip_downstream=True,
+            default_loop_retries=1,
             *args, **kwargs):
         super(CurwTriggerDagRunOperator, self).__init__(*args, **kwargs)
         self.python_callable = python_callable
         self.trigger_dag_id = trigger_dag_id
+        self.loop_id = loop_id
         self.skip_downstream = skip_downstream
+        self.default_loop_retries = default_loop_retries
+        self.loop_count = 1
 
-    def execute(self, context):
-        dro = DagRunOrder(run_id='trig__' + datetime.utcnow().isoformat())
-        dro = self.python_callable(context, dro)
-        if dro:
-            session = settings.Session()
-            dbag = DagBag(settings.DAGS_FOLDER)
-            trigger_dag = dbag.get_dag(self.trigger_dag_id)
-            dr = trigger_dag.create_dagrun(
-                run_id=dro.run_id,
-                state=State.RUNNING,
-                conf=dro.payload,
-                execution_date=context['execution_date'] if context['execution_date'] else None,
-                external_trigger=True)
-            self.log.info("Creating DagRun %s", dr)
-            session.add(dr)
-            session.commit()
-            session.close()
+    def execute(self, context, **kwargs):
+        """
+        if loop count > 0
+        :param context:
+        :param kwargs:
+        :return:
+        """
+        loop_count = self.get_loop_count()
+        if loop_count != 0:
+            dro = DagRunOrder(run_id='__'.join(['loop', self.trigger_dag_id, self.loop_id, loop_count]))
+            dro = self.python_callable(context, dro)
+            if dro:
+                self.log.info('')
+                session = settings.Session()
+                dbag = DagBag(settings.DAGS_FOLDER)
+                trigger_dag = dbag.get_dag(self.trigger_dag_id)
+                dr = trigger_dag.create_dagrun(
+                    run_id=dro.run_id,
+                    state=State.RUNNING,
+                    conf=dro.payload,
+                    execution_date=context['execution_date'] if context['execution_date'] else None,
+                    external_trigger=True)
+                self.log.info("Creating DagRun %s", dr)
+                session.add(dr)
+                session.commit()
+                session.close()
+
+                if loop_count > 0:
+                    self.log.info('Decrementing loop from %d' % loop_count)
+                    self.set_loop_count(loop_count - 1)
+
+                if self.skip_downstream:
+                    self.log.info('Skipping the downstream tasks')
+                    downstream_tasks = context['task'].get_flat_relatives(upstream=False)
+                    self.log.debug("Downstream task_ids %s", downstream_tasks)
+
+                    if downstream_tasks:
+                        self.skip(context['dag_run'], context['ti'].execution_date, downstream_tasks)
+            else:
+                self.log.info("Loop criteria not met. Continuing the downstream tasks")
+                self.delete_loop_count()
         else:
-            self.log.info("Criteria not met, moving on")
+            self.log.info('Loop count is 0. Continuing the downstream tasks ')
+
+    def delete_loop_count(self):
+        session = settings.Session()
+        meta = MetaData(session.connection(), reflect=True)
+        loop_table = meta.tables['loop']
+        try:
+            del_st = loop_table.delete().where(and_(loop_table.c.id == self.loop_id, loop_table.c.dag == self.dag_id))
+            session.execute(del_st)
+            session.commit()
+        finally:
+            session.close()
+
+    def get_loop_count(self):
+        session = settings.Session()
+        meta = MetaData(session.connection(), reflect=True)
+
+        loop_table = meta.tables['loop']
+        select_st = select([loop_table.c.count]).where(
+            and_(loop_table.c.id == self.loop_id, loop_table.c.dag == self.dag_id))
+        result = session.execute(select_st).fetchall()
+
+        try:
+            if len(result) == 0:
+                insert_st = loop_table.insert().values(id=self.loop_id, dag=self.dag_id,
+                                                       count=self.default_loop_retries)
+                session.execute(insert_st)
+                session.commit()
+                return self.default_loop_retries
+            elif len(result) == 1:
+                return dict(result[0])['count']
+            else:
+                raise CurwTriggerDagRunOperatorException(
+                    'Multiple entries %d in the loop table for %s %s' % (len(result), self.dag_id, self.loop_id))
+        finally:
+            session.close()
+
+    def set_loop_count(self, count):
+        session = settings.Session()
+        meta = MetaData(session.connection(), reflect=True)
+        loop_table = meta.tables['loop']
+
+        try:
+            update_st = update(loop_table).where(
+                and_(loop_table.c.id == self.loop_id, loop_table.c.dag == self.dag_id)).values(count=count)
+            session.execute(update_st)
+            session.commit()
+        finally:
+            session.close()
+
+        return count
+
+
+class CurwTriggerDagRunOperatorException(Exception):
+    pass
+
+#
+# class LoopEntry(Base):
+#     __tablename__ = "loop"
+#
+#     id = Column(String(250), primary_key=True)
+#     dag = Column(String(250), primary_key=True)
+#     count = Column(Integer())
+#
+#     __table_args__ = (
+#         Index('loop_id', id, dag, unique=True),
+#     )
