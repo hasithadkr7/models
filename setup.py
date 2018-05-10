dbeaf1285a80408ed8a4316ccf9126b3a7910b44
madumalt <madumalt@gmail.com>
2018-05-10 11:57:05 +0530

    disabled zip_safe install, due to resource inclusion

--- setup.py
+++ setup.py
@@ -11,5 +11,6 @@ setup(
     description='Models being developed at the Center for URban Water, Sri Lanka',
     include_package_data=True,
     requires=['airflow', 'shapely', 'joblib', 'netCDF4', 'matplotlib', 'imageio', 'scipy', 'geopandas'],
+    zip_safe=False
     # install_requires=['shapely', 'joblib', 'netCDF4', 'matplotlib', 'imageio', 'numpy', 'pandas', 'scipy']
 )
