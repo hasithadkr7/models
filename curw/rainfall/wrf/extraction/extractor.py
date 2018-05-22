ae808bae4f5b53d8b4c9c4fd518fce507142d6e9
niranda <niranda.perera@gmail.com>
2018-05-22 17:50:20 +0530

    adding flo2d and mike obs+forecast routines

--- curw/rainfall/wrf/extraction/extractor.py
+++ curw/rainfall/wrf/extraction/extractor.py
@@ -251,10 +251,16 @@ class CurwExtractorException(Exception):
     pass
 
 
-def extract_kelani_basin_rainfall_flo2d_with_obs(nc_f, adapter, obs_stations, output_dir, start_ts,
-                                                 duration_days=None,
-                                                 output_prefix='RAINCELL', ):
+def extract_kelani_basin_rainfall_flo2d_with_obs(nc_f, adapter, obs_stations, output_dir, start_ts, duration_days=None,
+                                                 output_prefix='RAINCELL', kelani_lower_basin_points=None,
+                                                 kelani_lower_basin_shp=None):
     """
+    :param duration_days:
+    :param kelani_lower_basin_points:
+    :param start_ts:
+    :param obs_stations:
+    :param adapter:
+    :param kelani_lower_basin_shp:
     :param output_prefix:
     :param nc_f:
     :param output_dir:
@@ -263,8 +269,11 @@ def extract_kelani_basin_rainfall_flo2d_with_obs(nc_f, adapter, obs_stations, ou
     if duration_days is None:
         duration_days = (2, 3)
 
-    kelani_lower_basin_points = res_mgr.get_resource_path('extraction/local/kelani_basin_points_250m.txt')
-    kelani_lower_basin_shp = res_mgr.get_resource_path('extraction/shp/klb-wgs84/klb-wgs84.shp')
+    if kelani_lower_basin_points is None:
+        kelani_lower_basin_points = res_mgr.get_resource_path('extraction/local/kelani_basin_points_250m.txt')
+
+    if kelani_lower_basin_shp is None:
+        kelani_lower_basin_shp = res_mgr.get_resource_path('extraction/shp/klb-wgs84/klb-wgs84.shp')
 
     points = np.genfromtxt(kelani_lower_basin_points, delimiter=',')
 
@@ -284,106 +293,93 @@ def extract_kelani_basin_rainfall_flo2d_with_obs(nc_f, adapter, obs_stations, ou
 
     t0 = dt.datetime.strptime(times[0], '%Y-%m-%d_%H:%M:%S')
     t1 = dt.datetime.strptime(times[1], '%Y-%m-%d_%H:%M:%S')
-    t_end = dt.datetime.strptime(times[-1], '%Y-%m-%d_%H:%M:%S')
 
     utils.create_dir_if_not_exists(output_dir)
-
-    # get observed data upto now
-
-    # stations in the area
-    # todo: update this to be dynamic
-    # obs_stations = adapter.get_stations_in_area(query={
-    #     'latitude_lower': str(kel_lat_min),
-    #     'longitude_lower': str(kel_lon_min),
-    #     'latitude_upper': str(kel_lat_max),
-    #     'longitude_upper': str(kel_lon_max),
-    # })
 
     obs_start = dt.datetime.strptime(start_ts, '%Y-%m-%d_%H:%M') - dt.timedelta(days=duration_days[0])
     obs_end = dt.datetime.strptime(start_ts, '%Y-%m-%d_%H:%M')
     forecast_end = dt.datetime.strptime(start_ts, '%Y-%m-%d_%H:%M') + dt.timedelta(days=duration_days[1])
 
-    obs = []
-    opts = {
-        'from': obs_start.strftime('%Y-%m-%d %H:%M:%S'),
-        'to': obs_end.strftime('%Y-%m-%d %H:%M:%S'),
-    }
+    obs = _get_observed_precip(obs_stations, obs_start, obs_end, duration_days, adapter)
 
-    def validate_ts(ts_sum):
-        if len(ts_sum) == duration_days[0] * 24 + 1:
+    thess_poly = spatial_utils.get_voronoi_polygons(obs_stations, kelani_lower_basin_shp, add_total_area=False)
+
+    output_file_path = os.path.join(output_dir, output_prefix + '.DAT')
+
+    # update points array with the thessian polygon idx
+    point_thess_idx = []
+    for point in points:
+        point_thess_idx.append(spatial_utils.is_inside_geo_df(thess_poly, lon=point[1], lat=point[2]))
+        pass
+
+    with open(output_file_path, 'w') as output_file:
+        res_mins = int((t1 - t0).total_seconds() / 60)
+        data_hours = int(sum(duration_days) * 24 * 60 / res_mins)
+        start_ts = utils.datetime_utc_to_lk(obs_start, shift_mins=30).strftime('%Y-%m-%d %H:%M:%S')
+        end_ts = utils.datetime_utc_to_lk(forecast_end, shift_mins=30).strftime('%Y-%m-%d %H:%M:%S')
+
+        output_file.write("%d %d %s %s\n" % (res_mins, data_hours, start_ts, end_ts))
+
+        for t in range(int(24 * 60 * duration_days[0] / res_mins) + 1):
+            for i, point in enumerate(points):
+                rf = float(obs[point_thess_idx[i]].values[t]) if point_thess_idx[i] is not None else 0
+                output_file.write('%d %.1f\n' % (point[0], rf))
+
+        forecast_start_idx = int(
+            np.where(times == utils.datetime_lk_to_utc(obs_end, shift_mins=30).strftime('%Y-%m-%d_%H:%M:%S'))[0])
+        for t in range(forecast_start_idx + 1, int(24 * 60 * duration_days[1] / res_mins)):
+            for point in points:
+                rf_x = np.digitize(point[1], lon_bins)
+                rf_y = np.digitize(point[2], lat_bins)
+                if t < len(times):
+                    output_file.write('%d %.1f\n' % (point[0], diff[t, rf_y, rf_x]))
+                else:
+                    output_file.write('%d %.1f\n' % (point[0], 0))
+
+
+def _get_observed_precip(obs_stations, start_dt, end_dt, duration_days, adapter):
+    def _validate_ts(_ts_sum):
+        if len(_ts_sum) == duration_days[0] * 24 + 1:
             return
 
         logging.warning('Validation count fails. Trying to fill zeros for missing values')
-        d = obs_start
-        ts_sum.sort_index()
-        for i in range(duration_days[0] * 24 + 1):
-            d = d + dt.timedelta(hours=i)
+        d = start_dt
+        _ts_sum.sort_index()
+        for j in range(duration_days[0] * 24 + 1):
+            d = d + dt.timedelta(hours=j)
             d_str = d.strftime('%Y-%m-%d %H:00')
-            if ts_sum.index[i] != d_str:
-                ts_sum.loc[d_str] = 0
-                ts_sum.sort_index()
+            if _ts_sum.index[j] != d_str:
+                _ts_sum.loc[d_str] = 0
+                _ts_sum.sort_index()
 
-        if len(ts_sum) == duration_days[0] * 24 + 1:
+        if len(_ts_sum) == duration_days[0] * 24 + 1:
             return
         else:
             raise CurwExtractorException('time series validation failed')
+
+    obs = {}
+    opts = {
+        'from': start_dt.strftime('%Y-%m-%d %H:%M:%S'),
+        'to': end_dt.strftime('%Y-%m-%d %H:%M:%S'),
+    }
 
     for s in obs_stations.keys():
         station = {'station': s,
                    'variable': 'Precipitation',
                    'unit': 'mm',
                    'type': 'Observed',
-                   'source': 'WeatherStation', }
+                   'source': 'WeatherStation',
+                   'name': obs_stations[s][2]
+                   }
 
         ts = np.array(adapter.retrieve_timeseries(station, opts)[0]['timeseries'])
         ts_df = pd.DataFrame(data=ts, columns=['ts', 'precip'], index=ts[0:])
         ts_sum = ts_df.groupby(by=[ts_df.ts.map(lambda x: x.strftime('%Y-%m-%d %H:00'))]).sum()
+        _validate_ts(ts_sum)
 
-        validate_ts(ts_sum)
+        obs[s] = ts_sum
 
-        obs.append(ts_sum)
-
-    thess_poly = spatial_utils.get_voronoi_polygons(obs_stations,
-                                                    kelani_lower_basin_shp,
-                                                    add_total_area=False)
-
-    pass
-
-    # prev_diff = []
-    # prev_days = len(nc_f_prev_days)
-    # for i in range(prev_days):
-    #     p_diff, _, _, _ = extract_area_rf_series(nc_f_prev_days[i], kel_lat_min, kel_lat_max, kel_lon_min, kel_lon_max)
-    #     prev_diff.append(p_diff)
-
-    # def write_forecast_to_raincell_file(output_file_path, alpha):
-    #     with open(output_file_path, 'w') as output_file:
-    #         res_mins = int((t1 - t0).total_seconds() / 60)
-    #         data_hours = int(len(times) + prev_days * 24 * 60 / res_mins)
-    #         start_ts = utils.datetime_utc_to_lk(t0 - dt.timedelta(days=prev_days), shift_mins=30).strftime(
-    #             '%Y-%m-%d %H:%M:%S')
-    #         end_ts = utils.datetime_utc_to_lk(t_end, shift_mins=30).strftime('%Y-%m-%d %H:%M:%S')
-    #
-    #         output_file.write("%d %d %s %s\n" % (res_mins, data_hours, start_ts, end_ts))
-    #
-    #         # for d in range(prev_days):
-    #         #     for t in range(int(24 * 60 / res_mins)):
-    #         #         for point in points:
-    #         #             rf_x = np.digitize(point[1], lon_bins)
-    #         #             rf_y = np.digitize(point[2], lat_bins)
-    #         #             output_file.write('%d %.1f\n' % (point[0], prev_diff[prev_days - 1 - d][t, rf_y, rf_x]))
-    #
-    #         for t in range(len(times)):
-    #             for point in points:
-    #                 rf_x = np.digitize(point[1], lon_bins)
-    #                 rf_y = np.digitize(point[2], lat_bins)
-    #                 if t < int(24 * 60 / res_mins):
-    #                     output_file.write('%d %.1f\n' % (point[0], diff[t, rf_y, rf_x] * alpha))
-    #                 else:
-    #                     output_file.write('%d %.1f\n' % (point[0], diff[t, rf_y, rf_x]))
-    #
-    # with TemporaryDirectory(prefix='curw_raincell') as temp_dir:
-    #     raincell_temp = os.path.join(temp_dir, output_prefix + '.DAT')
-    #     write_forecast_to_raincell_file(raincell_temp, 1)
+    return obs
 
 
 def create_rainfall_for_mike21(d0_rf_file, prev_rf_files, output_dir):
@@ -410,7 +406,55 @@ def create_rainfall_for_mike21(d0_rf_file, prev_rf_files, output_dir):
     with open(out_file, 'w') as out_f:
         for line in output:
             out_f.write('%s %s\t%s\n' % (line[0], line[1], line[2]))
-            # np.savetxt(out_file, output, fmt='%s', delimiter='\t')
+
+
+def create_rainfall_for_mike21_obs(d0_rf_file, adapter, obs_stations, output_dir, start_ts, duration_days=None,
+                                   kelani_lower_basin_shp=None):
+    if kelani_lower_basin_shp is None:
+        kelani_lower_basin_shp = res_mgr.get_resource_path('extraction/shp/klb-wgs84/klb-wgs84.shp')
+
+    if duration_days is None:
+        duration_days = (2, 3)
+
+    obs_start = dt.datetime.strptime(start_ts, '%Y-%m-%d_%H:%M') - dt.timedelta(days=duration_days[0])
+    obs_end = dt.datetime.strptime(start_ts, '%Y-%m-%d_%H:%M')
+    # forecast_end = dt.datetime.strptime(start_ts, '%Y-%m-%d_%H:%M') + dt.timedelta(days=duration_days[1])
+
+    obs = _get_observed_precip(obs_stations, obs_start, obs_end, duration_days, adapter)
+
+    thess_poly = spatial_utils.get_voronoi_polygons(obs_stations, kelani_lower_basin_shp, add_total_area=False)
+
+    observed = None
+    for i, _id in enumerate(thess_poly['id']):
+        if observed is not None:
+            observed = observed + obs[_id].astype(float) * thess_poly['area'][i]
+        else:
+            observed = obs[_id].astype(float) * thess_poly['area'][i]
+    observed = observed / sum(thess_poly['area'])
+
+    d0 = np.genfromtxt(d0_rf_file, dtype=str)
+    t0 = dt.datetime.strptime(' '.join(d0[0][:-1]), '%Y-%m-%d %H:%M:%S')
+    t1 = dt.datetime.strptime(' '.join(d0[1][:-1]), '%Y-%m-%d %H:%M:%S')
+
+    res_min = int((t1 - t0).total_seconds() / 60)
+
+    # prev_output = np.append(prev_output, d0, axis=0)
+    out_file = os.path.join(utils.create_dir_if_not_exists(output_dir), 'rf_mike21_obs.txt')
+
+    with open(out_file, 'w') as out_f:
+        for index in observed.index:
+            out_f.write('%s:00\t%.4f\n' % (index, observed.precip[index]))
+
+        forecast_start_idx = int(
+            np.where((d0[:, 0] == obs_end.strftime('%Y-%m-%d')) & (d0[:, 1] == obs_end.strftime('%H:%M:%S')))[0])
+        # note: no need to convert to utc as rf_mike21.txt has times in LK
+
+        for i in range(forecast_start_idx + 1, int(24 * 60 * duration_days[1] / res_min)):
+            if i < len(d0):
+                out_f.write('%s %s\t%s\n' % (d0[i][0], d0[i][1], d0[i][2]))
+            else:
+                out_f.write('%s\t0.0\n' % (obs_end + dt.timedelta(hours=i - forecast_start_idx - 1)).strftime(
+                    '%Y-%m-%d %H:%M:%S'))
 
 
 def extract_metro_col_rf_for_mike21(nc_f, output_dir, prev_rf_files=None, points_file=None):
@@ -1083,23 +1127,72 @@ class TestExtractor(unittest.TestCase):
         for f in files:
             out_dir = utils.create_dir_if_not_exists(
                 os.path.join(wrf_output_dir, f.replace('wrfout_d03', run_prefix).replace(':00_rf', '_0000'), 'wrf'))
-            shutil.copy2('/home/nira/Desktop/2018-05/%s' % f, out_dir)
+            shutil.copy2('/home/curw/Desktop/2018-05/%s' % f, out_dir)
 
         run_date = dt.datetime.strptime('2018-05-20_18:00', '%Y-%m-%d_%H:%M')
         now = '_'.join([run_prefix, run_date.strftime('%Y-%m-%d_%H:%M'), '*'])
 
         d03_nc_f = glob.glob(os.path.join(wrf_output_dir, now, 'wrf', 'wrfout_d03_*'))[0]
 
-        obs_stations = {'Kottawa North Dharmapala School': [79.95818, 6.865576],
-                        'IBATTARA2': [79.919, 6.908],
-                        'Malabe': [79.95738, 6.90396],
-                        'Mutwal': [79.8609, 6.95871]
-                        }
+        obs_stations = {'Kottawa North Dharmapala School': [79.95818, 6.865576, 'A&T Labs'],
+                        'IBATTARA2': [79.919, 6.908, 'CUrW IoT'],
+                        'Malabe': [79.95738, 6.90396, 'A&T Labs'],
+                        'Mutwal': [79.8609, 6.95871, 'A&T Labs']}
 
         start_ts = '2018-05-20_18:00'
         extract_kelani_basin_rainfall_flo2d_with_obs(d03_nc_f, adapter, obs_stations,
+                                                     os.path.join(wrf_output_dir, now, 'klb_flo2d'), start_ts, )
+
+    def test_extract_kelani_basin_rainfall_flo2d_obs_150m(self):
+        adapter = ext_utils.get_curw_adapter()
+        wrf_output_dir = tempfile.mkdtemp(prefix='flo2d_obs_')
+        files = ['wrfout_d03_2018-05-20_18:00:00_rf']
+        run_prefix = 'wrf0'
+
+        for f in files:
+            out_dir = utils.create_dir_if_not_exists(
+                os.path.join(wrf_output_dir, f.replace('wrfout_d03', run_prefix).replace(':00_rf', '_0000'), 'wrf'))
+            shutil.copy2('/home/curw/Desktop/2018-05/%s' % f, out_dir)
+
+        run_date = dt.datetime.strptime('2018-05-20_18:00', '%Y-%m-%d_%H:%M')
+        now = '_'.join([run_prefix, run_date.strftime('%Y-%m-%d_%H:%M'), '*'])
+
+        d03_nc_f = glob.glob(os.path.join(wrf_output_dir, now, 'wrf', 'wrfout_d03_*'))[0]
+
+        obs_stations = {'Kottawa North Dharmapala School': [79.95818, 6.865576, 'A&T Labs'],
+                        'IBATTARA2': [79.919, 6.908, 'CUrW IoT'],
+                        'Malabe': [79.95738, 6.90396, 'A&T Labs'],
+                        'Mutwal': [79.8609, 6.95871, 'A&T Labs'],
+                        'Glencourse': [80.20305, 6.97805, 'Irrigation Department'],
+                        }
+
+        start_ts = '2018-05-20_18:00'
+        kelani_lower_basin_points = res_mgr.get_resource_path('extraction/local/klb_glecourse_points_150m.txt')
+        kelani_lower_basin_shp = res_mgr.get_resource_path('extraction/shp/klb_glencourse/klb_glencourse.shp')
+        duration_days = (3, 3)
+        extract_kelani_basin_rainfall_flo2d_with_obs(d03_nc_f, adapter, obs_stations,
                                                      os.path.join(wrf_output_dir, now, 'klb_flo2d'), start_ts,
-                                                     )
+                                                     duration_days=duration_days,
+                                                     kelani_lower_basin_shp=kelani_lower_basin_shp,
+                                                     kelani_lower_basin_points=kelani_lower_basin_points,
+                                                     output_prefix='RAINCELL_150m')
+
+    def test_create_rainfall_for_mike21_obs(self):
+        adapter = ext_utils.get_curw_adapter()
+        wrf_output_dir = tempfile.mkdtemp(prefix='mike21_obs_')
+
+        out_dir = utils.create_dir_if_not_exists(os.path.join(wrf_output_dir, 'klb_mean_rf'))
+        shutil.copy2('/home/curw/Desktop/2018-05/klb_mean_rf/klb_mean_rf.txt', out_dir)
+
+        d0_mean_rf = os.path.join(out_dir, 'klb_mean_rf.txt')
+
+        obs_stations = {'Kottawa North Dharmapala School': [79.95818, 6.865576, 'A&T Labs'],
+                        'IBATTARA2': [79.919, 6.908, 'CUrW IoT'],
+                        'Malabe': [79.95738, 6.90396, 'A&T Labs'],
+                        'Mutwal': [79.8609, 6.95871, 'A&T Labs']}
+
+        start_ts = '2018-05-21_00:00'
+        create_rainfall_for_mike21_obs(d0_mean_rf, adapter, obs_stations, out_dir, start_ts, )
 
     def test_create_rainfall_for_mike21(self):
         wrf_output_dir = tempfile.mkdtemp(prefix='mike21_')
