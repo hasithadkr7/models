bc5346cbe012b5da2e65d321c89b96c4d584cea5
nirandaperera <niranda.perera@gmail.com>
2018-07-04 11:35:48 +0530

    changing airflow package name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email='niranda.17@cse.mrt.ac.lk',
     description='Models being developed at the Center for URban Water, Sri Lanka',
     include_package_data=True,
-    requires=['airflow', 'shapely', 'joblib', 'netCDF4', 'matplotlib', 'imageio', 'scipy', 'geopandas'],
+    requires=['apache-airflow', 'shapely', 'joblib', 'netCDF4', 'matplotlib', 'imageio', 'scipy', 'geopandas'],
     zip_safe=False
     # install_requires=['shapely', 'joblib', 'netCDF4', 'matplotlib', 'imageio', 'numpy', 'pandas', 'scipy']
 )
