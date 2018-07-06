7cd39dbb175b3266e1ef2dad5fcea2acaf770ace
niranda <niranda.perera@gmail.com>
2018-07-06 16:48:02 +0530

    curw dag run op - WIP

--- curw/workflow/airflow/extensions/operators/curw_dag_run_operator.py
+++ curw/workflow/airflow/extensions/operators/curw_dag_run_operator.py
@@ -12,15 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import datetime as dt
+import time
 from datetime import datetime
 
+from airflow import settings
 from airflow.models import BaseOperator, DagBag
 from airflow.utils.decorators import apply_defaults
 from airflow.utils.state import State
-from airflow import settings
-import datetime as dt
-from airflow.executors import GetDefaultExecutor
-
 
 
 class DagRunOrder(object):
@@ -33,7 +32,7 @@ class CurwDagRunOperator(BaseOperator):
     """
 
     """
-    template_fields = tuple()
+    template_fields = ('dag_run_conf', 'run_id', 'trigger_dag_id')
     template_ext = tuple()
     ui_color = '#ffefeb'
 
@@ -41,30 +40,46 @@ class CurwDagRunOperator(BaseOperator):
     def __init__(
             self,
             trigger_dag_id,
+            run_id=None,
+            dag_run_conf=None,
+            poll_interval=dt.timedelta(seconds=1),
+            wait_for_completion=False,
             *args, **kwargs):
         super(CurwDagRunOperator, self).__init__(*args, **kwargs)
         self.trigger_dag_id = trigger_dag_id
+        self.dag_run_conf = dag_run_conf
+        self.poll_interval = poll_interval
+        self.wait_for_completion = wait_for_completion
+        self.run_id = run_id
 
     def execute(self, context):
-        # run_id='trig__' + datetime.utcnow().isoformat()
-        #
-        # session = settings.Session()
-        # dbag = DagBag(settings.DAGS_FOLDER)
-        # trigger_dag = dbag.get_dag(self.trigger_dag_id)
-        # dr = trigger_dag.create_dagrun(
-        #     run_id=run_id,
-        #     state=State.RUNNING,
-        #     execution_date=context['execution_date'] + dt.timedelta(microseconds=1) if context[
-        #         'execution_date'] else None,
-        #     external_trigger=True)
-        # self.log.info("Creating DagRun %s", dr)
-        # session.add(dr)
-        # session.commit()
-        # session.close()
+        execution_time = datetime.utcnow()
+        self.log.info("Execution time %s", execution_time.isoformat())
+
+        if not self.run_id:
+            self.run_id = 'curw_trig__' + execution_time.isoformat()
+        self.log.info("DagRun Run ID %s", self.run_id)
 
         dbag = DagBag(settings.DAGS_FOLDER)
         trigger_dag = dbag.get_dag(self.trigger_dag_id)
-        ed = context['execution_date']
-        trigger_dag.run(
-            start_date=ed, end_date=ed, donot_pickle=True,
-            executor=GetDefaultExecutor())
+
+        dagrun = trigger_dag.create_dagrun(
+            run_id=self.run_id,
+            state=State.RUNNING,
+            execution_date=execution_time,
+            conf=self.dag_run_conf,
+            external_trigger=True)
+        self.log.info("Created DagRun %s \nConfig: %s" % (dagrun, str(self.dag_run_conf)))
+
+        while self.wait_for_completion and dagrun.get_state() is State.RUNNING:
+            self.log.info('Waiting for DagRun completion')
+            dagrun.update_state()
+            time.sleep(self.poll_interval.seconds)
+
+        #
+        # dbag = DagBag(settings.DAGS_FOLDER)
+        # trigger_dag = dbag.get_dag(self.trigger_dag_id)
+        # ed = context['execution_date']
+        # trigger_dag.run(
+        #     start_date=ed, end_date=ed, donot_pickle=True,
+        #     executor=GetDefaultExecutor())
